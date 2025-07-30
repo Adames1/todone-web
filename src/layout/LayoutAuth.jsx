dc6b8e22451e52e3974a623923787d0c201e2772
Adames1 <argenisadamesgarcia@gmail.com>
2025-07-30 14:48:55 -0400

    Etapa 1: Page login y estilos, vista responsive

--- src/layout/LayoutAuth.jsx
+++ src/layout/LayoutAuth.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import loginDraw from "../assets/undraw_login.svg";
 
-function LayoutAuth() {
-  return <div>LayoutAuth</div>;
+function LayoutAuth({ children }) {
+  return (
+    <div className="flex">
+      <div className="w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12">
+        <h2 className="text-lg font-medium text-black">ToDone</h2>
+        {children}
+      </div>
+
+      <div className="hidden md:flex w-[40vw] h-screen items-center justify-center bg-indigo-400 overflow-hidden p-8">
+        <img src={loginDraw} className="w-64 lg:w-[90%]" />
+      </div>
+    </div>
+  );
 }
 
 export default LayoutAuth;
--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,7 +1,62 @@
-import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+import LayoutAuth from "../../layout/LayoutAuth";
+import Input from "../../components/auth/Input";
 
 function Login() {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    // call here function of firebase
+  };
+
+  return (
+    <LayoutAuth>
+      <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
+        <h3 className="text-xl font-semibold text-black">
+          Bienvenido De Nuevo
+        </h3>
+        <p className="text-xs text-slate-700 mt-[5px] mb-6">
+          Por favor ingresa tus datos para iniciar sesión.
+        </p>
+
+        <form onSubmit={handleLogin}>
+          <Input
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}
+            label="Correo electronico"
+            placeholder="jhon@example.com"
+            type="text"
+          />
+          <Input
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+            label="Contraseña"
+            placeholder="Minimo 8 caracteres"
+            type="password"
+          />
+
+          <button type="submit" className="btn-primary">
+            ACCEDER
+          </button>
+
+          <p className="text-[13px] text-slate-800 mt-3">
+            ¿No tienes una cuenta?{" "}
+            <Link
+              className="font-medium text-primary underline"
+              to="/auth/register"
+            >
+              Registrate
+            </Link>
+          </p>
+        </form>
+      </div>
+    </LayoutAuth>
+  );
 }
 
 export default Login;
